19e7361ed28e90f0671ceb18e63f1f42329d92cf
NickTheFreak97 <niccolo.dellarocca@gmail.com>
2022-06-07 15:55:01 +0200

    Aggiunto progetto R e dati (parziale)

---
+++ R_data_analysis.R
@@ -0,0 +1,47 @@
+
+library(dplyr)
+library(ggpubr)
+
+df = read.csv("C:/Users/nicco/Documents/TeX/AI-Proj/data_new.csv")
+
+summarizedDf = df %>% group_by(Algorithm, Polygons)%>%
+  summarise(meanEBF = mean(EBF),
+            maxEBF = max(EBF),
+            minEBF = min(EBF),
+            meanRuntime = mean(Time),
+            maxRuntime = max(Time),
+            minRuntime = min(Time),
+            meanMemory = mean(Memory),
+            maxMemory = max(Memory),
+            minMemory = min(Memory),
+            meanFrontier = mean(Frontier),
+            maxFrontier = max(Frontier),
+            minFrontier = min(Frontier),
+            maxExplored = max(Explored),
+            minExplored = min(Explored),
+            meanExplored = mean(Explored),
+            avgVertices = mean(Avg.vertices),
+            Depth = mean(Depth),
+            .groups = 'drop')
+
+UC_Data <- summarizedDf[summarizedDf$Algorithm == 'UC',];
+A2_Data <- summarizedDf[summarizedDf$Algorithm == 'A*-d2',];
+A6_Data <- summarizedDf[summarizedDf$Algorithm == 'A*-d6',];
+
+UCCorrel <- UC_Data %>% summarize(correlazione = cor(meanEBF, Polygons)) %>% .$correlazione;
+A2Correl <- A2_Data %>% summarize(correlazione = cor(meanEBF, Polygons)) %>% .$correlazione;
+A6Correl <- A6_Data %>% summarize(correlazione = cor(meanEBF, Polygons)) %>% .$correlazione;
+
+UC_Data %>%
+  ggline(x="Polygons", y="meanEBF");
+
+A2_Data %>%
+  ggline(x="Polygons", y="meanEBF");
+
+A6_Data %>%
+  ggline(x="Polygons", y="meanEBF");
+
+df %>% 
+  group_by(Algorithm, Polygons) %>% 
+  ggline(x = "Polygons", y = "EBF", add = "mean_se", color = "Algorithm")
+
